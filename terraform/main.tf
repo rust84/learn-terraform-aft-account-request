02e0469eb10bee2b66a73b5811726f0c6fe97637
Russell Hall <rhall@greshamtech.com>
2024-05-09 15:35:27 +0100

    add aaren

--- terraform/main.tf
+++ terraform/main.tf
@@ -25,3 +25,31 @@ module "sandbox" {
 
   account_customizations_name = "sandbox"
 }
+
+module "aaren" {
+  source = "./modules/aft-account-request"
+
+  control_tower_parameters = {
+    AccountEmail              = "aft1@russellhall.co.uk"
+    AccountName               = "aaren-demo"
+    ManagedOrganizationalUnit = "Learn AFT"
+    SSOUserEmail              = "learn-aft@russellhall.co.uk"
+    SSOUserFirstName          = "Sandbox"
+    SSOUserLastName           = "AFT"
+  }
+
+  account_tags = {
+    "created-by" = "AFT"
+  }
+
+  change_management_parameters = {
+    change_requested_by = "HashiCorp Learn"
+    change_reason       = "Learn AWS Control Tower Account Factory for Terraform"
+  }
+
+  custom_fields = {
+    group = "non-prod"
+  }
+
+  account_customizations_name = "sandbox"
+}
